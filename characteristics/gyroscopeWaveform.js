0be5a7824537dd7cf34f3570832905cbc76fbad9
Sam Hendrickx <samhendrickx@gmail.com>
2016-05-03 11:30:44 +0200

    Gyroscope

--- characteristics/gyroscopeWaveform.js
+++ characteristics/gyroscopeWaveform.js
@@ -4,15 +4,54 @@ GYROSCOPE WAVEFORM
 Unfortunately no documentation...
 */
 
+var bufferpack = require('bufferpack');
+
+var angleX = 0;
+var angleY = 0;
+var angleZ = 0;
+
+function rotateX(angle) {
+  angleX += angle;
+}
+function rotateY(angle) {
+  angleY += angle;
+}
+function rotateZ(angle) {
+  angleZ += angle;
+}
+function convertGyro(gyro) {
+  var sampling_rate = 105;
+  var sensitivity = 500;
+  var half_range = 2 << 15;
+  var result = Math.round(gyro * sensitivity / half_range / sampling_rate * 10) / 10;;
+
+  return result;
+}
+function unpack(byte1, byte2) {
+  var byteArray = new Uint8Array(2);
+  byteArray[0] = byte1;
+  byteArray[1] = byte2;
+  var unpacked = bufferpack.unpack('<h', byteArray);
+
+  return unpacked[0];
+}
+
 function accelerationWaveform(characteristic) {
 
   characteristic.on('read', function(data, isNotification) {
-    if (data.length >= 12) {
-      var x = data[0] + data[1] * 255;
-      var y = data[2] + data[3] * 255;
-      var z = data[4] + data[5] * 255;
+    while (data.length >= 6) {
+      // Unpack
+      var x = unpack(data[0], data[1]);
+      var y = unpack(data[2], data[3]);
+      var z = unpack(data[4], data[5]);
+      // Convert gyro
+      rotateX(convertGyro(x));
+      rotateY(convertGyro(y));
+      rotateZ(convertGyro(z));
 
-      //console.log(x + ' ' + y + ' ' + z);
+      data = data.slice(6);
+
+      console.log('GYRO:\t' + x + ' ' + y + ' ' + z);
     }
 
   });
@@ -22,7 +61,5 @@ function accelerationWaveform(characteristic) {
   });
 
 }
-
-
 
 module.exports = accelerationWaveform;
